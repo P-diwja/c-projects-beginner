a56aa5ca26563c97fdbb5c8da1839f5e6b0034d1
P-diwja <pdiwja@gmail.com>
2024-10-14 21:18:47 +0530

    Add files via upload
    project 1 :
    A guessing game.

---
+++ zproject_1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number,guess,nguesses=1;
+    srand(time(0));
+    number=(rand()%100) + 1;
+    //generates a random no between 1 to 100.
+    //printf("the no is %d\n",number);
+    //keep running te loop tillthe no is guessed.
+    
+     printf(" !!! Guess the number !!!\n(between 1 to 100)\n");
+    do
+    {
+     scanf("%d",&guess);
+          if (guess>number){
+            printf("LOWER NUMBER PLEASE!!");
+          }
+          else if (guess<number)
+          {
+            printf("HIGHER NUMBER PLEASE!!");
+          }
+          else{
+             printf("YOU GUESSED IT IN %d ATTEMPTS\n",nguesses);
+          }
+          nguesses++;
+    }while(guess!=number);
+return 0;
+}
